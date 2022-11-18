161bd226dd1539a7e73c0d7a11529e2709a1811b
Lordfirespeed <joe@joeclack.me.uk>
2022-11-18 14:39:19 +0000

    refactor existing functionality into a ResponseHandler class

--- node_intro/app_2_electric_boogaloo.js
+++ node_intro/app_2_electric_boogaloo.js
@@ -5,40 +5,65 @@ const port = 9090;
 
 const server = http.createServer();
 
-function construct_response(headers, method, url, body, response) {
-    response.on('error', (err) => {
-        console.error(err);
-    });
+class RequestHandler {
+    constructor(request, response) {
 
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'application/json');
-    // Note: the 2 lines above could be replaced with this next one:
-    // response.writeHead(200, {'Content-Type': 'application/json'})
+        this.request = request;
+        this.response = response;
 
-    const responseBody = { headers, method, url, body };
+        this.headers = null;
+        this.method = null;
+        this.url = null;
+        this.body = [];
 
-    response.write(JSON.stringify(responseBody));
-    response.end();
-    // Note: the 2 lines above could be replaced with this next one:
-    // response.end(JSON.stringify(responseBody))
+        this.parse()
+        this.listen()
+    };
+
+    parse() {
+        const { headers, method, url } = this.request;
+        this.headers = headers;
+        this.method = method;
+        this.url = url;
+    }
+
+    listen() {
+        this.request.on('error', (err) => {
+            console.error(err);
+        });
+        this.request.on('data', (chunk) => {
+            this.body.push(chunk);
+        });
+        this.request.on('end', () => {
+            this.body = Buffer.concat(this.body).toString();
+            // At this point, we have the headers, method, url and body, and can now
+            // do whatever we need to in order to respond to this request.
+            this.construct_response();
+        });
+    }
+
+    construct_response() {
+        this.response.on('error', (err) => {
+            console.error(err);
+        });
+
+        this.response.statusCode = 200;
+        this.response.setHeader('Content-Type', 'application/json');
+        // Note: the 2 lines above could be replaced with this next one:
+        // response.writeHead(200, {'Content-Type': 'application/json'})
+
+        const responseBody = {headers: this.headers, url: this.url, method: this.method, body: this.body}
+
+        this.response.write(JSON.stringify(responseBody));
+        this.response.end();
+        // Note: the 2 lines above could be replaced with this next one:
+        // response.end(JSON.stringify(responseBody))
+    }
 }
 
 server.on('request', (request, response) => {
     // the same kind of magic happens here!
-    const { headers, method, url } = request;
-    let body = [];
-    request.on('error', (err) => {
-        console.error(err);
-    });
-    request.on('data', (chunk) => {
-        body.push(chunk);
-    });
-    request.on('end', () => {
-        body = Buffer.concat(body).toString();
-        // At this point, we have the headers, method, url and body, and can now
-        // do whatever we need to in order to respond to this request.
-        construct_response(headers, method, url, body, response)
-    });
+    new RequestHandler(request, response);
 });
 
 server.listen(port, hostname, () => {
