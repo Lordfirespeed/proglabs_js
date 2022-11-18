23dc34e49b1c8a8c04754beb676b0fe92afb9aab
Lordfirespeed <joe@joeclack.me.uk>
2022-11-18 14:23:50 +0000

    low-level request handling - error handling, read data stream, get headers + method

--- node_intro/app_2_electric_boogaloo.js
+++ node_intro/app_2_electric_boogaloo.js
@@ -7,6 +7,19 @@ const server = http.createServer();
 
 server.on('request', (request, response) => {
     // the same kind of magic happens here!
+    const { headers, method, url } = request;
+    let body = [];
+    request.on('error', (err) => {
+        console.error(err);
+    });
+    request.on('data', (chunk) => {
+        body.push(chunk);
+    });
+    request.on('end', () => {
+        body = Buffer.concat(body).toString();
+        // At this point, we have the headers, method, url and body, and can now
+        // do whatever we need to in order to respond to this request.
+    });
 });
 
 server.listen(port, hostname, () => {
