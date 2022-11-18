0981d40cabc9531f8f96e020b1bfffaa7e851754
Lordfirespeed <joe@joeclack.me.uk>
2022-11-18 14:27:44 +0000

    construct JSON response to requests

--- node_intro/app_2_electric_boogaloo.js
+++ node_intro/app_2_electric_boogaloo.js
@@ -5,6 +5,24 @@ const port = 9090;
 
 const server = http.createServer();
 
+function construct_response(headers, method, url, body, response) {
+    response.on('error', (err) => {
+        console.error(err);
+    });
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    // Note: the 2 lines above could be replaced with this next one:
+    // response.writeHead(200, {'Content-Type': 'application/json'})
+
+    const responseBody = { headers, method, url, body };
+
+    response.write(JSON.stringify(responseBody));
+    response.end();
+    // Note: the 2 lines above could be replaced with this next one:
+    // response.end(JSON.stringify(responseBody))
+}
+
 server.on('request', (request, response) => {
     // the same kind of magic happens here!
     const { headers, method, url } = request;
@@ -19,6 +37,7 @@ server.on('request', (request, response) => {
         body = Buffer.concat(body).toString();
         // At this point, we have the headers, method, url and body, and can now
         // do whatever we need to in order to respond to this request.
+        construct_response(headers, method, url, body, response)
     });
 });
 
